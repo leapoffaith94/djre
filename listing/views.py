6b1719b1bd9ac519e1652b831fc7f08f2f6088a7
leapoffaith94 <1034321716@qq.com>
2019-06-27 12:52:18 -0400

    day 1

---
+++ listing/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Listing
+
+
+def index(request):
+    listings = Listing.objects.all()
+    context = {
+        'listing': listings
+    }
+    return render(request, 'listings/listings.html')
+
+
+def listing(request):
+    return render(request, 'listings/listing.html')
+
+
+def search(request):
+    return render(request, 'listings/search.html')
